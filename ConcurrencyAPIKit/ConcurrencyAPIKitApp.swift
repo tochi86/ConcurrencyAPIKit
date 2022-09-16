a256c30698bcc05fa74708007ee77ea945b73b50
tochi86 <tochi86.ham@gmail.com>
2022-09-16 21:37:43 +0900

    Initial Commit

---
+++ ConcurrencyAPIKit/ConcurrencyAPIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConcurrencyAPIKitApp.swift
+//  ConcurrencyAPIKit
+//
+//  Created by Toshiya Kobayashi on 2022/09/16.
+//
+
+import SwiftUI
+
+@main
+struct ConcurrencyAPIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
